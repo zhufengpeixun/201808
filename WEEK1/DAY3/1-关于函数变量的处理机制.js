b78f81b9b69032a5380b2c74464f23cffd86e88e
zhouxiaotian <1282347298@qq.com>
2018-08-16 11:58:09 +0800

    20180816课堂讲义案例

--- WEEK1/DAY3/1-关于函数变量的处理机制.js
+++ WEEK1/DAY3/1-关于函数变量的处理机制.js
@@ -1,3 +1,4 @@
+/*
 console.log(n, m, x);
 var n = 12,
     m = 13,
@@ -12,3 +13,18 @@ function fn(n) {
 
 fn(10);
 console.log(n, m, x);
+*/
+
+console.log(x, y);//=>undefined*2
+var x = 10,
+    y = 20;
+
+function fn() {
+    console.log(x, y, z);//=>undefined 20 报错
+    var x = y = 100;
+    z = 30;
+    console.log(x, y, z);//=>100 100 30
+}
+
+x = fn(20);
+console.log(x, y, z);//=>undefined 100 30
